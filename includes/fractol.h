0d3114d412b841c333431782924f5283361ad157
Juan Carlos <flumperio@gmail.com>
2021-05-31 19:35:13 +0200

    31 - May - Home

---
+++ includes/fractol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juasanto <juasanto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/31 19:20:45 by juasanto          #+#    #+#             */
+/*   Updated: 2021/05/31 19:29:51 by juasanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+/*
+** Libraries
+*/
+# include <time.h>
+# include <math.h>
+# include "../Libft/includes/libft.h"
+# include "../minilibx/mlx.h"
+
+#endif
+
