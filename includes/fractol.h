c1f78500b71af33e8eee790b5f0330e2ec926050
Juan Carlos <flumperio@gmail.com>
2021-06-01 10:58:58 +0200

    1 - Jun - Home

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:20:45 by juasanto          #+#    #+#             */
-/*   Updated: 2021/05/31 19:29:51 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/06/01 10:10:57 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@
 # include <math.h>
 # include "../Libft/includes/libft.h"
 # include "../minilibx/mlx.h"
+/*
+** MLX
+*/
+typedef struct s_mlx
+{
+	void			*mlx_ptr;
+	void			*mlx_win;
+	void			*img;
+	char			*addr;
+	int				bits_per_pixel;
+	int				line_length;
+	int				endian;
+}					t_mlx;	
+
+typedef struct s_fra
+{
+	int				temp;
+	t_mlx			mlx;
+}					t_fra;
 
 #endif
 
--- sources/fractol.c
+++ sources/fractol.c
@@ -6,14 +6,47 @@
 /*   By: juasanto <juasanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:15:38 by juasanto          #+#    #+#             */
-/*   Updated: 2021/05/31 19:33:28 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/06/01 10:21:09 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
+void	free_all(t_fra *fra)
+{
+	int	cnt;
+
+	cnt = 0;
+	free(fra);
+}
+
+void	my_mlx_pixel_put(t_fra *fra, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = fra->mlx.addr + (y * fra->mlx.line_length
+			+ x * (fra->mlx.bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	init_mlx(t_fra *fra)
+{
+	fra->mlx.mlx_ptr = mlx_init();
+	fra->mlx.mlx_win = mlx_new_window(fra->mlx.mlx_ptr, 800, 600, "FraTol");
+}
+
 int		main ()
 {
+	t_fra	*fra;
+
+	fra = ft_calloc(sizeof(t_fra), 1);
+	fra->temp = 0;	
+
+
 	printf("Hola Majo que quieres de pincho");
+	free_all(fra);
+	system("leaks fractol");
 	return(0);
+
+
 }
